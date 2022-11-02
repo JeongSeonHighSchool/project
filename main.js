591db3a63f707040b87df0a5ea2af090e0970109
JeongSeonHighSchool <103098800+JeongSeonHighSchool@users.noreply.github.com>
2022-11-03 01:24:10 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 function video(){
     window.open("https://www.youtube.com/watch?v=34Ef_HgqIfM&ab_channel=%ED%95%B4%EC%96%91%EC%88%98%EC%82%B0%EB%B6%80%EC%96%B4%EC%84%9C%EC%98%A4%EC%85%98TV");
 }
+function docs(){
+    window.open("https://docs.google.com/forms/d/1iav-6SwpgdYI3K-Yo4-Ka18THNo53jPSVa9HWcVqwco/edit?pli=1");
+}
