20ae0f652defb544f3b8d6ce9636543886f4fb49
JeongSeonHighSchool <103098800+JeongSeonHighSchool@users.noreply.github.com>
2022-11-17 13:47:23 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -7,3 +7,6 @@ function docs(){
 function sub(){
     window.location = "sub.html";
 }
+function namu(){
+    window.open("https://namu.wiki/w/%ED%83%9C%ED%8F%89%EC%96%91%20%EA%B1%B0%EB%8C%80%20%EC%93%B0%EB%A0%88%EA%B8%B0%20%EC%A7%80%EB%8C%80");
+}
